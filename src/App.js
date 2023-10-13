998b6c28e671e96bf61eca7973ca2d201f0f250d
Danilo Giarlini <danilogiarlini@gmail.com>
2023-10-13 14:30:48 +0200

    almost finished, missing icon in button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import image from "./images/image-product-desktop.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="card">
+        <img src={image} alt="perfume bottle" />
+        <div className="card-content">
+          <h4>PERFUME</h4>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          <div className="price">
+            <h1>$149.99</h1>
+            <p>$169.99</p>
+          </div>
+          <button>Add to Cart</button>
+          
+        </div>
+      </div>
+    </>
   );
 }
 
